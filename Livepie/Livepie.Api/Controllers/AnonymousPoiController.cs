b101c47f6f4c83d240d247384a82ada5a21165bc
Steve van Bodegraven <svanbodegraven@live.com.au>
2017-07-17 10:57:45 +1000

    Working out CI/D for Livepie.Api...

--- Livepie/Livepie.Api/Controllers/AnonymousPoiController.cs
+++ Livepie/Livepie.Api/Controllers/AnonymousPoiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Livepie.Domain;
+using System.Net;
 
 namespace Livepie.Api.Controllers
 {
@@ -12,15 +13,22 @@ namespace Livepie.Api.Controllers
     {
         // GET api/AnonymousPoi
         [HttpGet]
-        public string Get([FromBody]Location location)
+        public int Get()
         {
-            return "value";
+            return (int)HttpStatusCode.OK;
         }
 
+        // GET api/AnonymousPoi
+        //[HttpGet]
+        //public string Get([FromBody]Location location)
+        //{
+        //    return "value";
+        //}
+
         // POST api/AnonymousPoi
-        [HttpPost]
-        public void Post([FromBody]Ping ping)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody]Ping ping)
+        //{
+        //}
     }
 }
